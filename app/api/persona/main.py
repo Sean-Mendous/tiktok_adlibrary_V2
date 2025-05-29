e10df61929b2e7c8d3e25a68f0c41bcf3c07a25f
sean <gongyouyongnatchikun@gmail.com>
2025-05-29 17:43:37 +0900

    changes the access limit

--- app/api/persona/main.py
+++ app/api/persona/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORSを許可
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tiktokvideo-r23y.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
